c895e2db17b0556fd8b1d1aa467e365bd2f13aae
romaklimenko <romaklimenko@gmail.com>
2023-06-03 00:07:50 +0200

    refactoring

--- app/posts.py
+++ app/posts.py
@@ -1,50 +1,42 @@
-# pylint: disable=missing-module-docstring, missing-function-docstring, missing-class-docstring
-from mongo import posts_collection, failures_collection
-from app import process_post, format_number
+# pylint: disable=missing-module-docstring, missing-function-docstring, missing-class-docstring, line-too-long, invalid-name
+import os
 import random
-import sys
 import time
 import traceback
+from datetime import timedelta
 
-from pymongo import ASCENDING, DESCENDING
+from pymongo import DESCENDING
 
-from dotenv import load_dotenv
-load_dotenv()
+from app import format_number, process_post
+from mongo import failures_collection, posts_collection
+
+process_start = int(time.time())
+print(timedelta(seconds=0), 'Начинаем обработку')
 
 
-max_errors = 10_000
+def get_timedelta():
+    return timedelta(seconds=time.time() - process_start)
+
+
 errors = 0
-
-from_id = 0
-to_id = 5 * 1_000_000
-
-skip_if_failed_before = False
 
 easing_threshold = int(time.time()) - (60 * 60 * 24) * 30
 
-# один аргумент - на сколько дней назад обновлять посты и комментарии: posts.py <days_ago>
-if len(sys.argv) == 2:
-    days_to_refresh = int(sys.argv[1])
-    recent_post = posts_collection.find({}).sort('id', DESCENDING).limit(1)[0]
-
+recent_post = posts_collection.find({}).sort('id', DESCENDING).limit(1)[0]
+recent_post_id = 1_000_000
+if recent_post is not None:
     recent_post_id = recent_post['id']
-    recent_post_created = recent_post['created']
+print(f'{get_timedelta()} Последний пост: https://d3.ru/{recent_post_id}/.')
 
-    from_id = list(
-        posts_collection
-        .find({'created': {'$gt': recent_post['created'] - days_to_refresh * 24 * 60 * 60}})
-        .sort('created', ASCENDING)
-        .limit(1))[0]['id']
+post_id_threshold = list(
+    posts_collection
+    .find({'created': {'$gt': recent_post['created'] - easing_threshold}})
+    .sort('created', DESCENDING)
+    .limit(1))[0]['id']
+print(f'{get_timedelta()} Пороговый id поста: {post_id_threshold}.')
 
-    posts_to_process = recent_post_id - from_id
-    max_errors = min(max_errors, int(posts_to_process / 10))
-
-    print(f"Обновляем посты с {from_id}")
-else:  # без аргументов - обновляем все посты и комментарии: posts.py
-    to_id = posts_collection.find({}).sort('id', DESCENDING).limit(1)[0]['id']
-    posts_to_process = to_id - from_id
-    skip_if_failed_before = True
-    failures_collection.delete_many({'failed': {'$lt': easing_threshold}})
+failures_collection.delete_many({'failed': {'$lt': easing_threshold}})
+print(f'{get_timedelta()} Удалили ошибки, которые произошли более порогового времени назад.')
 
 processed_posts = 0
 
@@ -97,74 +89,81 @@ def get_failures_to_skip():
     return failures_to_skip
 
 
-ids = list(range(from_id, to_id))
+post_ids = list(range(0, int(recent_post_id * 1.5)))
 
 posts_to_skip = set()
 failures_to_skip = set()
 
-if skip_if_failed_before:
-    failures_to_skip = get_failures_to_skip()
-    print(
-        f'Пропускаем посты, при получении которых произошли ошибки: {format_number(len(failures_to_skip))} шт.')
+failures_to_skip = get_failures_to_skip()
+print(
+    f'{get_timedelta()} Пропустили посты, при получении которых произошли ошибки: {format_number(len(failures_to_skip))} шт.')
 
-    posts_to_skip = get_posts_to_skip()
-    print(
-        f'Пропускаем посты, которые мы недавно обработали: {format_number(len(posts_to_skip))} шт.')
+posts_to_skip = get_posts_to_skip()
+print(
+    f'{get_timedelta()} Пропустили посты, которые мы недавно обработали: {format_number(len(posts_to_skip))} шт.')
 
-    def should_process(id):
-        PROBABILITY = 1 / 1000  # дадим небольшой шанс
 
-        if id in posts_to_skip:
-            posts_to_skip.remove(id)
-        elif f'post_id#{id}' in failures_to_skip:
-            failures_to_skip.remove(f'post_id#{id}')
-        else:
-            return True
+def should_process(post_id):  # pylint: disable=redefined-outer-name
+    if post_id in posts_to_skip:
+        posts_to_skip.remove(post_id)
+    elif f'post_id#{post_id}' in failures_to_skip:
+        failures_to_skip.remove(f'post_id#{post_id}')
+    elif post_id > post_id_threshold:
+        return True
+    else:
+        return True
 
-        return random.random() < PROBABILITY
+    # дадим небольшой шанс
+    return random.random() < 1 / int(os.getenv('ACTIVITIES_SKIP_PROBABILITY_DENOMINATOR', '1000'))
 
-    ids = list(filter(should_process, ids))
-    posts_to_process = len(ids)
 
-ids.sort()
+post_ids = list(filter(should_process, post_ids))
+posts_to_process_count = len(
+    list(filter(lambda post_id: post_id < recent_post_id, post_ids)))
+print(f'{get_timedelta()} Обработаем {format_number(posts_to_process_count)} постов.')
+max_errors = min(10_000, int(posts_to_process_count / 10))
+print(f'{get_timedelta()} Максимальное количество ошибок: {format_number(max_errors)}.')
 
 iteration_start = int(time.time())
 
-for id in ids:
+for post_id in post_ids:
     if errors > max_errors:
         break
 
     try:
         processed_posts += 1
 
-        if (processed_posts - posts_to_process) % 1000 == 0:
-            remaining_posts = format_number(processed_posts - posts_to_process)
-            processed_percent = round(processed_posts / posts_to_process * 100)
+        if (processed_posts - posts_to_process_count) % 1000 == 0:
+            remaining_posts = format_number(
+                processed_posts - posts_to_process_count)
+            processed_percent = round(
+                processed_posts / posts_to_process_count * 100)
             elapsed = max(int(time.time()) - iteration_start, 1)
-            posts_in_iteration = processed_posts - iteration_start_processed_posts
-            print(f"🐤 { remaining_posts } { processed_percent }% { str(elapsed).rjust(4, ' ') }sec { str(round(posts_in_iteration / elapsed)).rjust(4, ' ') }posts/sec")
+            print(f"{get_timedelta()} 🐤 { remaining_posts } { processed_percent }% { str(elapsed).rjust(4, ' ') }sec { str(round((processed_posts - iteration_start_processed_posts) / elapsed)).rjust(4, ' ') }posts/sec")
             iteration_start = int(time.time())
             iteration_start_processed_posts = processed_posts
 
-        (post, comments) = process_post(id)
+        (post, comments) = process_post(post_id)
 
-        failures_collection.delete_one({'_id': f'post_id#{id}'})
+        failures_collection.delete_one({'_id': f'post_id#{post_id}'})
 
         errors = 0
-    except Exception as e:
+    except Exception as e:  # pylint: disable=broad-except
         traceback_str = traceback.format_exc()
         failures_collection.replace_one(
-            {'_id': f'post_id#{id}'},
+            {'_id': f'post_id#{post_id}'},
             {
-                '_id': f'post_id#{id}',
+                '_id': f'post_id#{post_id}',
                 'error': str(e),
                 'traceback': traceback_str,
                 'failed': int(time.time())
             }, upsert=True)
-        if processed_posts > posts_to_process:
+        if processed_posts > posts_to_process_count:
             processed_posts -= 1
         if str(e) != '404':
-            print(f"🐤 ❌ { format_number(processed_posts - posts_to_process) } { round(processed_posts / posts_to_process * 100) }% https://d3.ru/{id}/\nОшибка: {e}\nОставшиеся попытки: {format_number(max_errors - errors)}\n")
+            print(f"{get_timedelta()} 🐤 ❌ { format_number(processed_posts - posts_to_process_count) } { round(processed_posts / posts_to_process_count * 100) }% https://d3.ru/{post_id}/\nОшибка: {e}\nОставшиеся попытки: {format_number(max_errors - errors)}\n")
             print(traceback_str)
         errors += 1
         continue
+
+print(timedelta(seconds=time.time() - process_start), 'Обработка завершена')
